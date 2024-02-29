b9f59e231796b2b26057dade3505ec71e152c6b5
CFeeney333 <c.feeney333@gmail.com>
2024-02-29 10:35:57 +0000

    Change term staff member to staff user

--- src/Staff.java
+++ src/Staff.java
@@ -4,35 +4,35 @@
 public class Staff {
 
     /**
-     * The first name of the staff member
+     * The first name of the staff user
      */
     private String firstName;
     /**
-     * The last name of the staff member
+     * The last name of the staff user
      */
     private String lastName;
     /**
-     * The staff member's email address
+     * The staff user's email address
      */
     private String email;
     /**
-     * The staff member's phone number
+     * The staff user's phone number
      */
     private String phone;
     /**
-     * The staff member's password for their account
+     * The staff user's password for their account
      */
     private String password;
 
 
     /**
-     * Create a Staff member
+     * Create a staff user
      *
-     * @param firstName the staff member's first name
-     * @param lastName  the staff member's surname
-     * @param email     the staff member's email address
-     * @param phone     the staff member's phone number
-     * @param password  the password for the staff member's account
+     * @param firstName the staff user's first name
+     * @param lastName  the staff user's surname
+     * @param email     the staff user's email address
+     * @param phone     the staff user's phone number
+     * @param password  the password for the staff user's account
      */
     public Staff(String firstName, String lastName, String email, String phone, String password) {
         this.firstName = firstName;
@@ -44,90 +44,90 @@ public class Staff {
 
 
     /**
-     * Get the staff member's first name
+     * Get the staff user's first name
      *
-     * @return the staff member's first name
+     * @return the staff user's first name
      */
     public String getFirstName() {
         return firstName;
     }
 
     /**
-     * Set the staff member's first name
+     * Set the staff user's first name
      *
-     * @param firstName the staff member's first name
+     * @param firstName the staff user's first name
      */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
     /**
-     * Get the staff member's last name
+     * Get the staff user's last name
      *
-     * @return the staff member's last name
+     * @return the staff user's last name
      */
     public String getLastName() {
         return lastName;
     }
 
     /**
-     * Set the staff member's last name
+     * Set the staff user's last name
      *
-     * @param lastName the staff member's last name
+     * @param lastName the staff user's last name
      */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
     /**
-     * Get the staff member's email address
+     * Get the staff user's email address
      *
-     * @return the staff member's email address
+     * @return the staff user's email address
      */
     public String getEmail() {
         return email;
     }
 
     /**
-     * Set the staff member's email address
+     * Set the staff user's email address
      *
-     * @param email the staff member's email address
+     * @param email the staff user's email address
      */
     public void setEmail(String email) {
         this.email = email;
     }
 
     /**
-     * Get the staff member's phone number
+     * Get the staff user's phone number
      *
-     * @return the staff member's phone number
+     * @return the staff user's phone number
      */
     public String getPhone() {
         return phone;
     }
 
     /**
-     * Set the staff member's phone number
+     * Set the staff user's phone number
      *
-     * @param phone the staff member's phone number
+     * @param phone the staff user's phone number
      */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
     /**
-     * Get the staff member's password
+     * Get the staff user's password
      *
-     * @return the staff member's password
+     * @return the staff user's password
      */
     public String getPassword() {
         return password;
     }
 
     /**
-     * Set the staff member's password
+     * Set the staff user's password
      *
-     * @param password the staff member's password
+     * @param password the staff user's password
      */
     public void setPassword(String password) {
         this.password = password;
