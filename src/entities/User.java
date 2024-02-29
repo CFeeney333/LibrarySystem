5ad7edd96cb0932ab9396fa61d31a581f551669e
ChippySpud <c.feeney333@gmail.com>
2024-02-29 13:55:55 +0000

    Add javadoc to User variables and constructor

--- src/entities/User.java
+++ src/entities/User.java
@@ -2,13 +2,44 @@ package entities;
 
 abstract class User {
 
+    /**
+     * The user's id
+     */
     private long id;
+    /**
+     * The user's first name
+     */
     private String firstName;
+    /**
+     * The user's last name
+     */
     private String lastName;
+    /**
+     * The user's password
+     */
     private String password;
+    /**
+     * The user's phone number
+     */
     private String phone;
+    /**
+     * The user's email address
+     */
     private String email;
 
+    /**
+     * Create a User of a library system
+     * <p>
+     * A User is someone who has an account on a library system
+     * </p>
+     *
+     * @param id        the user's id
+     * @param firstName the user's first name
+     * @param lastName  the user's last name
+     * @param password  the user's password
+     * @param phone     the user's phone number
+     * @param email     the user's email address
+     */
     public User(long id, String firstName, String lastName, String password, String phone, String email) {
         this.id = id;
         this.firstName = firstName;
