42c8365574ed5552477ebc8f64df5817150850fd
CFeeney333 <c.feeney333@gmail.com>
2024-02-29 11:10:29 +0000

    Update Member and Staff to extend the User class

--- src/entities/Member.java
+++ src/entities/Member.java
@@ -6,159 +6,22 @@ package entities;
  * A Member refers to someone who has an account with the library for borrowing books.
  * </p>
  */
-public class Member {
+public class Member extends User {
 
     /**
-     * The member user's id
-     */
-    private long id;
-    /**
-     * The first name of the member user
-     */
-    private String firstName;
-    /**
-     * The last name of the member user
-     */
-    private String lastName;
-    /**
-     * The member user's password for their account
-     */
-    private String password;
-    /**
-     * The member user's phone number
-     */
-    private String phone;
-    /**
-     * The member user's email address
-     */
-    private String email;
-
-
-    /**
-     * Create a Member user
+     * Create a Member user of a library system
+     * <p>
+     * A Member is someone who borrows and returns books
+     * </p>
      *
      * @param id        the member user's id
      * @param firstName the member user's first name
      * @param lastName  the member user's last name
-     * @param password  the password for the member user's account
+     * @param password  the member user's password
      * @param phone     the member user's phone number
      * @param email     the member user's email address
      */
     public Member(long id, String firstName, String lastName, String password, String phone, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.phone = phone;
-        this.email = email;
-    }
-
-
-    /**
-     * Get the member user's id
-     *
-     * @return the member user's id
-     */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * Set the member user's id
-     *
-     * @param id the member user's id
-     */
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    /**
-     * Get the member user's first name
-     *
-     * @return the member user's first name
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Set the member user's first name
-     *
-     * @param firstName the member user's first name
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Get the member user's last name
-     *
-     * @return the member user's last name
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Set the member user's last name
-     *
-     * @param lastName the member user's last name
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Get the member user's password
-     *
-     * @return the member user's password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * Set the member user's password
-     *
-     * @param password the member user's password
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * Get the member user's phone number
-     *
-     * @return the member user's phone number
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    /**
-     * Set the member user's phone number
-     *
-     * @param phone the member user's phone number
-     */
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    /**
-     * Get the member user's email address
-     *
-     * @return the member user's email address
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Set the member user's email address
-     *
-     * @param email the member user's email address
-     */
-    public void setEmail(String email) {
-        this.email = email;
+        super(id, firstName, lastName, password, phone, email);
     }
 }
--- src/entities/Staff.java
+++ src/entities/Staff.java
@@ -3,159 +3,22 @@ package entities;
 /**
  * A member of Staff of a Library
  */
-public class Staff {
+public class Staff extends User {
 
     /**
-     * The staff user's id
-     */
-    private long id;
-    /**
-     * The first name of the staff user
-     */
-    private String firstName;
-    /**
-     * The last name of the staff user
-     */
-    private String lastName;
-    /**
-     * The staff user's password for their account
-     */
-    private String password;
-    /**
-     * The staff user's phone number
-     */
-    private String phone;
-    /**
-     * The staff user's email address
-     */
-    private String email;
-
-
-    /**
-     * Create a Staff user
+     * Create a Staff user of a library system
+     * <p>
+     * A Staff is an administrator of the library system
+     * </p>
      *
      * @param id        the staff user's id
      * @param firstName the staff user's first name
-     * @param lastName  the staff user's surname
-     * @param password  the password for the staff user's account
+     * @param lastName  the staff user's last name
+     * @param password  the staff user's password
      * @param phone     the staff user's phone number
      * @param email     the staff user's email address
      */
     public Staff(long id, String firstName, String lastName, String password, String phone, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.phone = phone;
-        this.email = email;
-    }
-
-
-    /**
-     * Get the staff user's id
-     *
-     * @return the staff user's id
-     */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * Set the staff user's id
-     *
-     * @param id the staff user's id
-     */
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    /**
-     * Get the staff user's first name
-     *
-     * @return the staff user's first name
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Set the staff user's first name
-     *
-     * @param firstName the staff user's first name
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Get the staff user's last name
-     *
-     * @return the staff user's last name
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Set the staff user's last name
-     *
-     * @param lastName the staff user's last name
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Get the staff user's email address
-     *
-     * @return the staff user's email address
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Set the staff user's email address
-     *
-     * @param email the staff user's email address
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Get the staff user's phone number
-     *
-     * @return the staff user's phone number
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    /**
-     * Set the staff user's phone number
-     *
-     * @param phone the staff user's phone number
-     */
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    /**
-     * Get the staff user's password
-     *
-     * @return the staff user's password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * Set the staff user's password
-     *
-     * @param password the staff user's password
-     */
-    public void setPassword(String password) {
-        this.password = password;
+        super(id, firstName, lastName, password, phone, email);
     }
 }
