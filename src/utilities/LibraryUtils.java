9c93f7a52bece5066b2d9a05ea46a334cb285b65
CFeeney333 <c.feeney333@gmail.com>
2024-04-16 15:37:47 +0100

    Add comments to the LibraryUtils class

--- src/utilities/LibraryUtils.java
+++ src/utilities/LibraryUtils.java
@@ -4,7 +4,21 @@ import entities.Staff;
 
 import java.util.ArrayList;
 
+/**
+ * This class contains a collection of static utility methods for use by the Library System
+ */
 public class LibraryUtils {
+
+    /**
+     * Create a nice readable representation of an ArrayList of Staff members
+     *
+     * <p>
+     * Uses the staffListItem method to create a nice readable String representation of an ArrayList of Staff members
+     * </p>
+     *
+     * @param staffArrayList the ArrayList of staff members to make a list of
+     * @return the String representation of the ArrayList
+     */
     public static String orderedStaffList(ArrayList<Staff> staffArrayList) {
         StringBuilder list = new StringBuilder();
         for (int i = 0; i < staffArrayList.size(); i++) {
@@ -14,6 +28,18 @@ public class LibraryUtils {
         return list.toString();
     }
 
+    /**
+     * Create a nice readable representation of a Staff member
+     *
+     * <p>
+     * This method creates a nice readable String representation of a Staff member for display in the system. It
+     * intentionally does not include the password in the output, and the first name and last name are joined to make
+     * the full name
+     * </p>
+     *
+     * @param staff the staff member to get the String representation of
+     * @return the String representation of the staff member
+     */
     public static String staffListItem(Staff staff) {
         return "\t\tID: " + staff.getId() + "\n" +
                 "\t\tName: " + staff.getFirstName() + " " + staff.getLastName() + "\n" +
