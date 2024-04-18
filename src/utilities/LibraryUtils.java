59fc5427f659c9826e16d8ba481536509993788d
CFeeney333 <c.feeney333@gmail.com>
2024-04-18 11:00:06 +0100

    Add methods for listing members to the LibraryUtils class

--- src/utilities/LibraryUtils.java
+++ src/utilities/LibraryUtils.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import entities.Member;
 import entities.Staff;
 
 import java.util.ArrayList;
@@ -29,6 +30,25 @@ public class LibraryUtils {
     }
 
     /**
+     * Create a nice readable representation of an ArrayList of Member users
+     *
+     * <p>
+     * Users the memberListItem method to create a nice readable String representation of an ArrayList of Member users
+     * </p>
+     *
+     * @param memberArrayList the ArrayList of member users to make a list of
+     * @return the String representation of the ArrayList
+     */
+    public static String orderedMemberList(ArrayList<Member> memberArrayList) {
+        StringBuilder list = new StringBuilder();
+        for (int i = 0; i < memberArrayList.size(); i++) {
+            Member m = memberArrayList.get(i);
+            list.append(i + 1).append(":").append(memberListItem(m));
+        }
+        return list.toString();
+    }
+
+    /**
      * Create a nice readable representation of a Staff member
      *
      * <p>
@@ -46,5 +66,26 @@ public class LibraryUtils {
                 "\t\tUsername: " + staff.getUserName() + "\n" +
                 "\t\tPhone Number: " + staff.getPhone() + "\n" +
                 "\t\tEmail: " + staff.getEmail() + "\n";
+    }
+
+    /**
+     * Create a nice readable representation of a member user
+     *
+     * <p>
+     * This method creates a nice readable String representation of a Member user for display in the system. It
+     * intentionally does not include the password in the output, and the first name and last name are joined to make
+     * the full name
+     * </p>
+     *
+     * @param member the member user to get the String representation of
+     * @return the String representation of the member user
+     */
+    public static String memberListItem(Member member) {
+        return "\t\tID: " + member.getId() + "\n" +
+                "\t\tName: " + member.getFirstName() + " " + member.getLastName() + "\n" +
+                "\t\tUsername: " + member.getUserName() + "\n" +
+                "\t\tPhone Number: " + member.getPhone() + "\n" +
+                "\t\tEmail: " + member.getEmail() + "\n";
+
     }
 }
